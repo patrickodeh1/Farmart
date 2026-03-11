1ae97cf2a6c846615872bc2a197d70034b3d5c05
Patrick Odeh <vyltyan@gmail.com>
2026-03-11 19:03:54 +0100

    Register Rezgo listener to OrderPlacedEvent

--- main/app/Providers/EventServiceProvider.php
+++ main/app/Providers/EventServiceProvider.php
@@ -2,8 +2,20 @@
 
 namespace App\Providers;
 
+use App\Listeners\SubmitOrderToRezgo;
+use Botble\Ecommerce\Events\OrderPlacedEvent;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        OrderPlacedEvent::class => [
+            SubmitOrderToRezgo::class,
+        ],
+    ];
 }
